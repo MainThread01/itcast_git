ef56a46f59499553139a36c9bd89b4f3d7c8f7ee
HDF <987867420@qq.com>
2020-04-19 00:51:25 +0800

    新建User,.java

---
+++ maven_day01_demo/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * Author: Dave
+ * Date: 2020/4/19
+ * Time: 0:40
+ * Description:
+ */
+public class User {
+    private Integer id;
+    private String name;
+}
